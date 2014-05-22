e6726128d10c86c4bbec84662e09c7005c6976d8
Richard Bormann <Richard.Bormann@ipa.fraunhofer.de>
2014-05-22 10:16:07 +0200

    update

--- autopnp_scenario/scripts/localize.py
+++ autopnp_scenario/scripts/localize.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import roslib
-#roslib.load_manifest('autopnp_scenario')
+roslib.load_manifest('autopnp_scenario')
 import rospy
 import tf
 import os, sys, math
@@ -8,15 +8,30 @@ import imreg, numpy
 
 from nav_msgs.msg import OccupancyGrid, GridCells
 from std_msgs.msg import String
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped, PoseArray
 from sensor_msgs.msg import Joy
+from std_srvs.srv import Empty
+from cob_sound.srv import SayText, SayTextRequest
 from simple_script_server import simple_script_server
 
+from exploration_detection_cleaning import currentRobotPose
+
 def say(sen):
+	print(sen)
 	rospy.wait_for_service('/say')
-	say = rospy.ServiceProxy('/say', String)
+	say_ = rospy.ServiceProxy('/say', SayText)
+	s = SayTextRequest()
+	s.text = sen
 	try:
-	  resp1 = say(sen)
+	  resp1 = say_(s)
+	except rospy.ServiceException as exc:
+	  print("Service did not process request: " + str(exc))
+
+def global_localization():
+	rospy.wait_for_service('/global_localization')
+	s = rospy.ServiceProxy('/global_localization', Empty)
+	try:
+	  resp1 = s()
 	except rospy.ServiceException as exc:
 	  print("Service did not process request: " + str(exc))
 
@@ -43,16 +58,15 @@ def send_base_pose(x, y, rot, var_trans, var_rot):
 0, 0, 0, 0, 0, 0,
 0, 0, 0, 0, 0, var_rot]
 
-	print "publishing new robot pose"
-	pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped)
-	pub.publish(pwcs)
+	global pose_pub
+	pose_pub.publish(pwcs)
 
 def OccupancyGrid2Im(m):
 	#normalize
 	d = []
 	for i in xrange(len(m.data)):
 		v=0.
-		if m.data[i]>50 or m.data[i]<0: v=1.
+		if m.data[i]>90 or m.data[i]<-10: v=1.
 		d.append( v )
 	return numpy.swapaxes(numpy.asarray(d).reshape([m.info.height, m.info.width]),0,1)
 
@@ -65,17 +79,13 @@ def cb_joy(msg):
 	if msg.buttons[0]==0 and msg.buttons[1]==1 and msg.buttons[2]==0 and msg.buttons[3]==0 and msg.buttons[4]==0 and msg.buttons[5]==1 and msg.buttons[6]==0 and msg.buttons[7]==1 and msg.buttons[8]==0 and msg.buttons[9]==0 and msg.buttons[10]==0 and msg.buttons[11]==0:
 		confirm = True
 
-def cb_map(m):
-	global im_map
-	global confirm
-	im_map = OccupancyGrid2Im(m)
-	imreg.imshow(im_map)
+done = 0
 
-	return
-	#now seems up, so turn around
+def init():
+	global confirm
 	sss = simple_script_server()
-	say("I am ready. I will initialize myself. Then I will turn around.")
-	say("Please onfirm")
+	say("I am ready. I will initialize myself.")
+	say("Please confirm.")
 	confirm = False
 	while confirm==False: rospy.sleep(0.1)
 
@@ -101,65 +111,72 @@ def cb_map(m):
 	# recover components
 	handle_head = sss.recover("head")
 	if handle_head.get_error_code() != 0:
-		return say("failed to recover head")		
+		return say("failed to recover head")
 
-	#turn around
-	for x in xrange(8): sss.move_base_rel('base', [0,0,math.pi/8])
+	return True
 
-	#ready
-	im_map = tmp
+def cb_map(m):
+	global done
+	if done>0: return
 
-def cb_local(m):
-	global im_map
-	if im_map==None: return
+	if init()!=True: return
 
-	#now both (map + local_map) are present
-	mia=[0,0,0,0]
-	for c in m.cells:
-		x = int(c.x/cell_width)
-		y = int(c.y/cell_width)
-		mia = [min(x,mia[0]), min(y,mia[1]), max(x,mia[2]), max(y,mia[3]) ]
-	d=[]
-	for x in xrange(mia[2]-mia[0]):
-		d.append([])
-		for y in xrange(mia[3]-mia[1]):
-			d[len(d)-1].append(0.)
-	for c in m.cells:
-		x = int(c.x/cell_width)
-		y = int(c.y/cell_width)
-		d[x][y]=1.
-	im_local = numpy.asarray(d)
-	say("Starting to localize myself")
+	global_localization()
+	done = 1
 
-	imreg.imshow(im_map, im_local, 0.125)
+def cb_particles(msg):
+	global done
+	if done!=1: return
+	arr_x=[]
+	arr_y=[]
+	arr_yaw=[]
+	for p in msg.poses:
+		quaternion = (p.orientation.x, p.orientation.y, p.orientation.z, p.orientation.w)
+		euler = tf.transformations.euler_from_quaternion(quaternion)
+		arr_x.append(p.position.x)
+		arr_y.append(p.position.y)
+		arr_yaw.append(euler[2])
 
-	#registrate images
-	match = imreg.find_match(im_map, im_local)
-	print(match)
-	if match==None:
-		say("Ccould not localize. Abort.")
-	say("Found a match with a score of "+str(match[3]))
+	m = numpy.mean(numpy.asarray(arr_yaw), axis=0)
+	for i in xrange(len(arr_yaw)):
+		d = abs(arr_yaw[i]-m)
+		if d>math.pi: arr_yaw[i]=d-math.pi
 
-	#get pose
-	x = match[0]*m.info.resolution
-	y = match[1]*m.info.resolution
-	yaw = match[2]*math.pi/180.
+	dx = numpy.std(numpy.asarray(arr_x), axis=0)
+	dy = numpy.std(numpy.asarray(arr_y), axis=0)
+	dyaw=numpy.std(numpy.asarray(arr_yaw), axis=0)
 
-	print("Pose: ",x,y,yaw)
-	send_base_pose(x,y,yaw, 0.1, 0.1)
+	print(dx,dy,dyaw)
 
-	#check for user
-	(rx,ry,ryaw,rname) = rospy.get_param('relation', [0.,0.,0., "origin"])
-	say("I am thinking that I am standing in relation to "+rname)
-	dist = math.sqrt( (rx-x)*(rx-x) + (ry-y)*(ry-y) )
-	angle = abs(yaw-ryaw)*180/math.pi
-	say("Distance: "+str("%.1f" % dist))
-	say("Delta angle: "+str(round(angle)))
+	if dx<0.2 and dy<0.2 and dyaw<0.2:
+		say("I am now localized. Checking.")
+		done = 2
+
+		listener = tf.TransformListener()
+		(trans,quat,rot) = currentRobotPose()
+
+		print (trans, rot)
+		x = trans[0]
+		y = trans[1]
+		yaw = rot[0]
+		
+		#check for user
+		(rx,ry,ryaw,rname) = rospy.get_param('relation', [0.,0.,0., "origin"])
+		say("I am thinking that I am standing in relation to "+rname)
+		dist = math.sqrt( (rx-x)*(rx-x) + (ry-y)*(ry-y) )
+		angle = abs(yaw-ryaw)*180/math.pi
+		say("Distance: "+str("%.1f" % dist)+" meters")
+		say("Delta angle: "+str(int(round(angle)))+" degrees")
+
+		done = 3
+		exit(0)
 
 if __name__ == '__main__':
 	rospy.init_node('localize')
+	pose_pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped)
 	rospy.Subscriber("/map", OccupancyGrid, cb_map)
-	rospy.Subscriber("/obstacles", GridCells, cb_local)
+	rospy.Subscriber('/particlecloud', PoseArray, cb_particles)
+	#rospy.Subscriber("/local_costmap_node/costmap/obstacles", GridCells, cb_local)
 	rospy.Subscriber("/joy", Joy, cb_joy)
 	rospy.spin()
 	
